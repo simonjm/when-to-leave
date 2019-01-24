64df913cddd60547022b0e7de2e3fbdff4763640
Joe Simon <simonjm09@gmail.com>
2019-01-24 17:38:17 -0600

    Added delay calculation

--- index.js
+++ index.js
@@ -18,13 +18,32 @@ function fetchDeparture(url) {
         .then(data => {
             const $ = cheerio.load(data);
             const nextDeparture = $('.nextDepartureText .countdown').text();
-            return nextDeparture
+            if (nextDeparture === '') {
+                throw new Error('Could not find departure text');
+            }
+            const match = /^(\d+) Min/.exec(nextDeparture);
+            if (match === null) {
+                throw new Error('Regex failed to match string: ' + nextDeparture);
+            }
+
+            return parseInt(match[1], 10);
         });
+}
+
+// TODO: Trains run every 10 min during rush hour. Figure out a way to handle times when trains run slower
+const trainInterval = 10;
+function calculateLeaveTime(departure, delay) {
+    const leaveIn = departure - delay;
+    if (leaveIn >= 0) {
+        return leaveIn;
+    }
+
+    return trainInterval + leaveIn;
 }
 
 app.get('/to/work', (req, res) => {
     fetchDeparture(toWorkUrl)
-        .then(departure => res.type('html').status(200).send(`<p>${departure}</p>`))
+        .then(departure => res.type('html').status(200).send(`<p>${calculateLeaveTime(departure, 5)}</p>`))
         .catch(err => res.status(500).send(`Failed to fetch departure. ${err.toString()}`));
 });
 
